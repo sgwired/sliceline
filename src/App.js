792753b9d061b5144095e166ecc43477dffefad8
Shelton Gayles <sgbythelake@gmail.com>
2019-11-10 00:00:48 -0800

    install google fonts

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
-    background-color: blue;
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
   }
 `;
 
 function App() {
   return (
     <>
+      <h1>Sliceline</h1>
       <GlobalStyle />
       <div>Hello Sliceline</div>
     </>
